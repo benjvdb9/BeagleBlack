b421ecc39dea1cf78b537f4392908fc92b3aa757
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2017-03-30 10:50:54 +0200

    Adapted existing code to work without Adafruit

--- InitLED.py
+++ InitLED.py
@@ -2,72 +2,113 @@ import Adafruit_BBIO.GPIO as GPIO
 
 #initialize pins
 
+def getpin(P):
+	dict = {'R1':'44', 'R2':'26', 'R3':'46', 'R4':'65', 'R5':'63', 'R6':'37', 'R7':'33', 'R8':'61'
+			'C1':'45', 'C2':'23', 'C3':'47', 'C4':'27', 'C5':'22', 'C6':'62', 'C7':'36', 'C8':'32'}
+	
+	try:
+		pin = dict[P]
+	except:
+		pin = P
+	
+	return pin
+	
+def pinconf(P, dir):
+	pin = getpin(P)
+	
+	if not os.path.exists('/sys/class/gpio/gpio' + pin):  
+	    with open('/sys/class/gpio/export', 'w') as file:  
+	        file.write(pin)  
+	with open('/sys/class/gpio/gpio'+ pin +'/direction', 'w') as file:  
+	    file.write(dir)
+	
+def pin(P, state):
+	pin = getpin(P)
+	
+	if (state == 'on'):
+		frt = 1
+	else:
+		frt = 0
+		
+	with open('/sys/class/gpio/gpio'+ pin +'/value', 'w') as file:  
+	    file.write(frt)
+ 
+def readpin(P):
+	pin = getpin(P)
+	
+	if not os.path.exists('/sys/class/gpio/gpio' + pin):  
+	    with open('/sys/class/gpio/export', 'w') as file:  
+	        file.write(pin)
+	with open('/sys/class/gpio/gpio'+ pin +'/value', 'w') as file:  
+	    val = file.read()
+		return val
+
 def initGPIOpins():
-	GPIO.setup("P8_12", GPIO.OUT)
-	GPIO.setup("P8_14", GPIO.OUT)
-	GPIO.setup("P8_16", GPIO.OUT)
-	GPIO.setup("P8_18", GPIO.OUT)
+	pinconf('R1', out)
+	pinconf('R2', out)
+	pinconf('R3', out)
+	pinconf('R4', out)
 
-	GPIO.setup("P8_20", GPIO.OUT)
-	GPIO.setup("P8_22", GPIO.OUT)
-	GPIO.setup("P8_24", GPIO.OUT)
-	GPIO.setup("P8_26", GPIO.OUT)
+	pinconf('R5', out)
+	pinconf('R6', out)
+	pinconf('R7', out)
+	pinconf('R8', out)
 
 
-	GPIO.setup("P8_11", GPIO.OUT)
-	GPIO.setup("P8_13", GPIO.OUT)
-	GPIO.setup("P8_15", GPIO.OUT)
-	GPIO.setup("P8_17", GPIO.OUT)
+	pinconf('C1', out)
+	pinconf('C2', out)
+	pinconf('C3', out)
+	pinconf('C4', out)
 
-	GPIO.setup("P8_19", GPIO.OUT)
-	GPIO.setup("P8_21", GPIO.OUT)
-	GPIO.setup("P8_23", GPIO.OUT)
-	GPIO.setup("P8_25", GPIO.OUT)
+	pinconf('C1', out)
+	pinconf('C2', out)
+	pinconf('C3', out)
+	pinconf('C4', out)
 
 def inputUpperON():
-	GPIO.output("P8_12", GPIO.HIGH)
-	GPIO.output("P8_14", GPIO.HIGH)
-	GPIO.output("P8_16", GPIO.HIGH)
-	GPIO.output("P8_18", GPIO.HIGH)
+	pin('R1', on)
+	pin('R2', on)
+	pin('R3', on)
+	pin('R4', on)
 
 def inputLowerON():
-	GPIO.output("P8_20", GPIO.HIGH)
-	GPIO.output("P8_22", GPIO.HIGH)
-	GPIO.output("P8_24", GPIO.HIGH)
-	GPIO.output("P8_26", GPIO.HIGH)
+	pin('R5', on)
+	pin('R6', on)
+	pin('R7', on)
+	pin('R8', on)
 	
 def inputUpperOFF():
-	GPIO.output("P8_12", GPIO.LOW)
-	GPIO.output("P8_14", GPIO.LOW)
-	GPIO.output("P8_16", GPIO.LOW)
-	GPIO.output("P8_18", GPIO.LOW)
+	pin('R1', off)
+	pin('R2', off)
+	pin('R3', off)
+	pin('R4', off)
 
 def inputLowerOFF():
-	GPIO.output("P8_20", GPIO.LOW)
-	GPIO.output("P8_22", GPIO.LOW)
-	GPIO.output("P8_24", GPIO.LOW)
-	GPIO.output("P8_26", GPIO.LOW)
+	pin('R5', off)
+	pin('R6', off)
+	pin('R7', off)
+	pin('R8', off)
 	
 def outputUpperON():
-	GPIO.output("P8_11", GPIO.HIGH)
-	GPIO.output("P8_13", GPIO.HIGH)
-	GPIO.output("P8_15", GPIO.HIGH)
-	GPIO.output("P8_17", GPIO.HIGH)
+	pin('C1', on)
+	pin('C2', on)
+	pin('C3', on)
+	pin('C4', on)
 	
 def outputLowerON():
-	GPIO.output("P8_19", GPIO.HIGH)
-	GPIO.output("P8_21", GPIO.HIGH)
-	GPIO.output("P8_23", GPIO.HIGH)
-	GPIO.output("P8_25", GPIO.HIGH)
+	pin('C5', on)
+	pin('C6', on)
+	pin('C7', on)
+	pin('C8', on)
 	
 def outputUpperOFF():
-	GPIO.output("P8_11", GPIO.LOW)
-	GPIO.output("P8_13", GPIO.LOW)
-	GPIO.output("P8_15", GPIO.LOW)
-	GPIO.output("P8_17", GPIO.LOW)
+	pin('C1', off)
+	pin('C2', off)
+	pin('C3', off)
+	pin('C4', off)
 
 def outputLowerOFF():
-	GPIO.output("P8_19", GPIO.LOW)
-	GPIO.output("P8_21", GPIO.LOW)
-	GPIO.output("P8_23", GPIO.LOW)
-	GPIO.output("P8_25", GPIO.LOW)
+	pin('C5', off)
+	pin('C6', off)
+	pin('C7', off)
+	pin('C8', off)
--- LEDmax.py
+++ LEDmax.py
@@ -1,4 +1,3 @@
-import Adafruit_BBIO.GPIO as GPIO
 from initLED import initGPIOpins, inputUpperON
 from initLED import inputLowerON, outputUpperOFF, outputLowerOFF
 
